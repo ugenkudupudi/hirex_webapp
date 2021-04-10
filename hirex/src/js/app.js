b01db6ab3089b7e3616a90dd14d2622e59817de9
ugenkudupudi <ugen@kudupudi.com>
2021-04-10 14:22:37 +0530

    version v0.1
    Virgin appstack code

---
+++ hirex/src/js/app.js
@@ -0,0 +1,28 @@
+// Global
+import "./modules/bootstrap";
+import "./modules/theme";
+import "./modules/dragula";
+import "./modules/feather";
+import "./modules/moment";
+import "./modules/sidebar";
+import "./modules/notyf";
+
+// Charts
+import "./modules/chartjs";
+import "./modules/apexcharts";
+
+// Forms
+import "./modules/daterangepicker";
+import "./modules/datetimepicker";
+import "./modules/fullcalendar";
+import "./modules/mask";
+import "./modules/quill";
+import "./modules/select2";
+import "./modules/validation";
+import "./modules/wizard";
+
+// Maps
+import "./modules/vector-maps";
+
+// Tables
+import "./modules/datatables";
